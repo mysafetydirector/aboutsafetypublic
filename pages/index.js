2c01ecc96e51f811aa8cb71826983c0683e3e526
mysafetydirector <mysafetydirector@gmail.com>
2018-07-05 07:53:47 -0500

    Added MSc after name

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ function Component() {
             />
           </div>
           <h2 className="subtitle">
-            OSHA Compliant Safety Training<br />With Ed Light, CSP
+            OSHA Compliant Safety Training<br />With Ed Light, MSc, CSP
           </h2>
         </div>
         <br />
